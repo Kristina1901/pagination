a076c2695cbd9a9ca375b6fd25d5ede56e167601
Kristina <ktuzova1901@gmail.com>
2022-02-15 03:04:39 +0200

    pagination

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
+import NewApiService from './news-service'
+import articlesTpl from './templates/articles.hbs'
 
-const DEBOUNCE_DELAY = 300;
+
+
+const formEl = document.querySelector(".form")
+const input = document.getElementById('search-box')
+const show = document.querySelector(".show")
+const container = document.querySelector(".container")
+const newsApiService = new NewApiService();
+formEl.addEventListener('submit', onSearch)    
+function onSearch(e) {
+  e.preventDefault();
+    clearArticlesContainer()
+    newsApiService.query = input.value
+    newsApiService.resetPage()
+    newsApiService.fetchArticles().then(appendArtticleMarkup)
+   }
+show.addEventListener('click', onLoadMore)
+function onLoadMore() {
+  newsApiService.fetchArticles().then(appendArtticleMarkup)
+}
+function appendArtticleMarkup(articles) {
+    container.insertAdjacentHTML('beforeend', articlesTpl
+        (articles));
+}
+function clearArticlesContainer() {
+  container.innerHTML = ''
+}
---
+++ src/news-service.js
@@ -0,0 +1,35 @@
+export default class NewsApiService{
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+      
+    } 
+    fetchArticles() {
+       const options = {
+    headers: {
+       Authorization: 'fd723a94ec2b4e4eabf33f8f9c11678f',
+    }
+    }
+    const url =`https://newsapi.org/v2/everything?q=${this.searchQuery}&pageSize=5&language=en&page=${this.page}`
+    return fetch(url, options)
+    .then(r => r.json())
+    .then(data => {
+        this.incrementPage()
+        
+        return data.articles
+        })
+    
+    }
+    incrementPage() {
+        this.page += 1
+    }
+    resetPage() {
+        this.page = 1
+    }
+    get query() {
+        return this.searchQuery
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery
+    }
+}
